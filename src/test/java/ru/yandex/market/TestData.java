f5819cfa25d653834b3899f3b0507b1a1b65eb66
arkadykiselev <qwerty12>
2020-04-04 20:58:50 +0300

    Added phone model

--- src/test/java/ru/yandex/market/TestData.java
+++ src/test/java/ru/yandex/market/TestData.java
@@ -4,5 +4,5 @@ package ru.yandex.market;
  * Created by A on 04.04.2020 20:18
  */
 public class TestData {
-    public static final String PHONE = "";
+    public static final String PHONE = "Redmi Note 8 Pro 6/64GB";
 }
